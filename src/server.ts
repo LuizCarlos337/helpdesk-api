f415f2b97abfa16d90ebd141c20ead09e302ca84
Luiz Junior <luizoliveira9380@gmail.com>
2026-08-29 18:46:04 -0300

    chore: initialize helpdesk api

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+
+import { buildApp } from './app.js'
+
+const app = buildApp();
+
+const port = Number(process.env.PORT ?? 3333);
+const host = process.env.HOST ?? '0.0.0.0';
+
+async function start() {
+    try {
+        await app.listen({
+            port, 
+            host,
+        })
+    } catch(error) {
+        app.log.error(error);
+        process.exit(1)
+    }
+}
+
+start()
